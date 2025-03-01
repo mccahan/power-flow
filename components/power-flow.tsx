bcc0a64bf36f7e5cb95e8f87b08d3fc2f2e1c473
Ryan McCahan <ryan@mccahan.net>
2025-03-01 00:05:28 -0700

    Fix battery-to-grid flow

--- components/power-flow.tsx
+++ components/power-flow.tsx
@@ -222,7 +222,7 @@ export default function PowerFlow({
     gridToBattery: gridStatus === "1" && gridPower > 10 && batteryPower < -10 && Math.abs(batteryPower) > solarPower - homePower,
     gridToHome: gridStatus === "1" && gridPower > 10 && homePower > solarPower + batteryPower,
     batteryToHome: batteryPower > 10 && homePower > solarPower,
-    batteryToGrid: gridPower < -10 && batteryPower > 10 && Math.abs(gridPower) >= homePower - solarPower,
+    batteryToGrid: gridPower < -10 && batteryPower > 10 && Math.abs(gridPower) >= homePower - solarPower - batteryPower,
   }
   const solarOnly = SOLAR_ONLY
 
